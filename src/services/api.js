0ef9bc80b39a3b1b8812a17dd4e4aab9ecb4c96a
ajaysingh143473 <ajaysingh143473@gmail.com>
2026-04-28 23:31:12 +0530

    service api modified

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080/api"
+    baseURL: process.env.REACT_APP_API_URL
+        ? `${process.env.REACT_APP_API_URL}/api`
+        : "http://localhost:8080/api"
 });
 
 export const explainCode = (data) => API.post("/explain", data);
